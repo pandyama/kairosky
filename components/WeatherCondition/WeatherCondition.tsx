c89ecac6f6b0e57d37afc951cfda66d6a085084a
pandyama <pandyama2@gmail.com>
2025-08-03 20:44:29 -0600

    Weather condition component added

---
+++ components/WeatherCondition/WeatherCondition.tsx
@@ -0,0 +1,53 @@
+import { Text, View } from 'react-native';
+import { styles } from './WeatherConditionStyle';
+import React from 'react';
+import { Precipitation, Wind, Sunrise, Sunset } from '../Icons';
+
+interface Conditions {
+  feelsLike: string;
+  wind: string;
+  humidity: string;
+  high: string;
+  low: string;
+  sunrise: any;
+  sunset: any;
+}
+
+export default function WeatherCondition({
+  // feelsLike,
+  wind,
+  // humidity,
+  // high,
+  // low,
+  sunrise,
+  sunset,
+}: Conditions) {
+  return (
+    <View>
+      <View style={styles.row}>
+        <View style={styles.wind}>
+          <Wind size={64} />
+          <Text style={{ ...styles.text, fontSize: 20 }}>{wind} km/h</Text>
+        </View>
+        <View style={styles.wind}>
+          <Precipitation size={64} />
+          <Text style={{ ...styles.text, fontSize: 20 }}>70%</Text>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={{ ...styles.wind, backgroundColor: '#FAD961' }}>
+          <Text style={{ ...styles.text, fontSize: 20 }}>{sunrise}</Text>
+          <Sunrise size={48} />
+        </View>
+
+        <View style={{ ...styles.wind, backgroundColor: '#0F2027' }}>
+          <Sunset size={48} />
+          <Text style={{ ...styles.text, fontSize: 20, color: '#FFFFFF' }}>
+            {sunset}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ components/WeatherCondition/WeatherConditionStyle.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    marginTop: 10,
+    gap: 15, // adds spacing between children
+    paddingHorizontal: 16, // 👈 moves the whole row inward
+  },
+  wind: {
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    flex: 1,
+    aspectRatio: 1,
+    backgroundColor: '#C2E9FB',
+  },
+  text: {
+    fontFamily: 'RadioCanada-Regular',
+    color: 'black',
+  },
+});
