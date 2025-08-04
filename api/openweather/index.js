a9b80365a2c1617de2f88c0e7e71d86f3e076671
pandyama <pandyama2@gmail.com>
2025-08-03 20:08:09 -0600

    api logic

--- api/openweather/index.js
+++ api/openweather/index.js
@@ -1,38 +1,38 @@
-import axios from 'axios'
-import moment from 'moment'
-import Constants from 'expo-constants'
-import { getWeatherIcon } from '../../common'
-const countriesList = require('./countries.json')
+import axios from 'axios';
+import moment from 'moment';
+import { getWeatherIcon } from '../../common';
+const countriesList = require('./countries.json');
 
-const { getLocalTimeAndDate } = require('../../common/util')
+const { getLocalTimeAndDate } = require('../../common/util');
 
 export const weather = async (city, lat, lon) => {
-  const key = Constants.expoConfig.extra.API_KEY
-  let apiResponse
-  let geoApiResponse
+  // const key = Constants.expoConfig.extra.API_KEY
+  const key = '0fe37647bf3c4095418a1c5392bb60cc';
+  let apiResponse;
+  let geoApiResponse;
 
   if (lat && lon) {
     try {
       apiResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`
-      )
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`,
+      );
       geoApiResponse = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`
-      )
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`,
+      );
     } catch (e) {
-      return { success: false }
+      return { success: false };
     }
   } else if (city) {
     try {
       apiResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
-      )
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`,
+      );
 
       geoApiResponse = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`
-      )
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`,
+      );
     } catch (e) {
-      return { success: false }
+      return { success: false };
     }
   }
 
@@ -40,29 +40,29 @@ export const weather = async (city, lat, lon) => {
     date: localDate,
     time: localTime,
     timestamp: localTimeStamp,
-  } = getLocalTimeAndDate(apiResponse.data.timezone)
+  } = getLocalTimeAndDate(apiResponse.data.timezone);
 
-  let sunriseMoment
-  sunriseMoment = moment.unix(apiResponse.data.sys.sunrise)
-  sunriseMoment = sunriseMoment.utcOffset(apiResponse.data.timezone / 3600)
-  const formattedSunriseTime = sunriseMoment.format('hh:mm A')
+  let sunriseMoment;
+  sunriseMoment = moment.unix(apiResponse.data.sys.sunrise);
+  sunriseMoment = sunriseMoment.utcOffset(apiResponse.data.timezone / 3600);
+  const formattedSunriseTime = sunriseMoment.format('hh:mm A');
 
-  let sunsetMoment
-  sunsetMoment = moment.unix(apiResponse.data.sys.sunset)
-  sunsetMoment = sunsetMoment.utcOffset(apiResponse.data.timezone / 3600)
-  const formattedSunsetTime = sunsetMoment.format('hh:mm A')
+  let sunsetMoment;
+  sunsetMoment = moment.unix(apiResponse.data.sys.sunset);
+  sunsetMoment = sunsetMoment.utcOffset(apiResponse.data.timezone / 3600);
+  const formattedSunsetTime = sunsetMoment.format('hh:mm A');
 
-  console.log('🚀 >> weather >> formattedSunsetTime>>', formattedSunsetTime)
+  console.log('🚀 >> weather >> formattedSunsetTime>>', formattedSunsetTime);
 
-  const weatherCode = apiResponse.data.weather[0].id
+  const weatherCode = apiResponse.data.weather[0].id;
   const icon =
     moment(localTimeStamp).hour() > 6 && moment(localTimeStamp).hour() < 19
       ? getWeatherIcon(weatherCode)
-      : 'sleeping'
+      : 'sleeping';
 
   const countryName = countriesList.filter(
-    (item) => item.code === geoApiResponse.data[0].country
-  )
+    item => item.code === geoApiResponse.data[0].country,
+  );
 
   const weatherStat = {
     cityName: apiResponse.data.name,
@@ -79,6 +79,6 @@ export const weather = async (city, lat, lon) => {
     time: localTime,
     date: localDate,
     icon,
-  }
-  return { data: weatherStat, success: true }
-}
+  };
+  return { data: weatherStat, success: true };
+};
